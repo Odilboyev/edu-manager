1ab9f007596ec217037a36c4d73d2d875eac48e4
odilboyev <webiusdev@gmail.com>
2021-11-18 20:40:43 +0500

    responsibility

--- js/index.js
+++ js/index.js
@@ -2,26 +2,30 @@ let checkbox = document.querySelector("#mode");
 let body = document.querySelector("body");
 let checkbox__wrapper = document.querySelector(".navbar-nav__switch");
 let sun__icon = document.querySelector("#sun");
-
+let moon__icon = document.querySelector("#moon");
 if (checkbox.checked) {
   checkbox__wrapper.style.background = "#ed9526";
   sun__icon.style.opacity = "0";
   body.className = "dark";
+  moon__icon.style.display = "block";
 } else {
   checkbox__wrapper.style.background = "#fff";
   sun__icon.style.opacity = "1";
   body.className = "light";
+  moon__icon.style.display = "none";
 }
 
 checkbox.addEventListener("change", function () {
   if (this.checked) {
     checkbox__wrapper.style.background = "#ed9526";
-    sun__icon.style.opacity = "0";
+    sun__icon.style.display = "none";
     body.className = "dark";
+    moon__icon.style.display = "block";
   } else {
     checkbox__wrapper.style.background = "#fff";
-    sun__icon.style.opacity = "1";
+    sun__icon.style.display = "block";
     body.className = "light";
+    moon__icon.style.display = "none";
   }
 });
 
@@ -38,6 +42,8 @@ var swiper = new Swiper(".mySwiper", {
   autoplay: 2500,
   autoplayDisableOnInteraction: true,
   slidesPerView: 1.3,
+  observer: true,
+  observeParents: true,
   breakpoints: {
     320: {
       loop: false,
@@ -45,7 +51,7 @@ var swiper = new Swiper(".mySwiper", {
     },
     1024: {
       loop: true,
-      slidesPerView: 1.3,
+      slidesPerView: 1.5,
     },
   },
 });
